cb1c5b5e7c39944e3b67be53201986f2bc2e9f8a
Evgen Kiktenko <31948994+kick10o@users.noreply.github.com>
2017-09-14 10:37:57 +0300

    Add files via upload
    online html css learning in process

---
+++ firsttry.js
@@ -0,0 +1,7 @@
+alert("welcome!");
+
+var VisitorsName = prompt("What is your name?");
+var VisitorsFavouriteColour = prompt("What is your Favourite Colour?");
+
+document.write("Hello "+ VisitorsName + " Your Favourite Colour is "+ VisitorsFavouriteColour);
+console.log ("Program completed!");
---
+++ years.js
@@ -0,0 +1,4 @@
+var VisitorAge = prompt("How many years you have been alive");
+var YearsOfLife = parseInt(VisitorAge);
+YearsOfLife = YearsOfLife*365;
+document.write("You have been living for  "+YearsOfLife+"Days!!");
